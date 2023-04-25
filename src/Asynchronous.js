26242f43bf1b9b2f0c827b9824b3df58f7c56799
Umair <mrumairhassan@outlook.com>
2023-04-25 16:51:52 +0500

    This is POST previous is Get

--- src/Asynchronous.js
+++ src/Asynchronous.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 export default function Asynchronous() {
 
+
+//GET Example 
+
 fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log('This is GET',json));
+
+
+// POST Example
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log('This is post' ,json));
+
+
   return (
     <div>
     
